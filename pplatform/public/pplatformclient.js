a4158adc47ccc1ec92e2ece2a2f932f6fb7ef3d1
Luz <devluz@live.de>
2016-01-14 21:54:10 +1300

    added shared message system in platform + a system to change the shown game together with all connected devices

--- pplatform/public/pplatformclient.js
+++ pplatform/public/pplatformclient.js
@@ -1,35 +1,73 @@
 //pure platform interface
 //contains still some jquery commands accessing the GUI they will move into the index.html in the future
     
+    
+ function Controller()
+ {
+     this.id = -1;
+     this.name = "player -1";
+ }
  function PPlatform()
  {
+     
+     var self = this;
     var gConnected = false;
+    
+    var mKnownConnections = null;
+    
+    var TAG_CONTROLLER_DISCOVERY = "PLATFORM_CONTROLLER_DISCOVERY";
+    var TAG_CONTROLLER_LEFT = "PLATFORM_CONTROLLER_LEFT";
+    var TAG_ENTER_GAME = "PLATFORM_ENTER_GAME";
+    var TAG_EXIT_GAME = "PLATFORM_EXIT_GAME";
+    
+    var mActiveGame = null;
     
     var serverUrl = "http://because-why-not.com:3001";
     if(window.location.href.indexOf("localhost") > -1) {
         serverUrl = "http://localhost:3001";
     }
     
-    var mMessageListener = [];
+    var mMessageListener = [OnUserMessage];
     var sigChan = new Netgroup(serverUrl);
     
+    var mIsView = false;
+    var mIsHostController = false;
     
+    this.isView = function()
+    {
+        return mIsView;
+    }
+    this.isController = function()
+    {
+        return mIsView == false;
+    }
+    this.isHostController = function()
+    {
+        return mIsHostController;
+    }
     
+    this.enterGame = function(lGame)
+    {
+        this.sendMessage(TAG_ENTER_GAME, lGame);
+    };
     
     this.startAsView = function()
     {
         
         var key = GetRandomKey();
         
-        Log("Opening room " + key + " ...");
+        self.Log("Opening room " + key + " ...");
         sigChan.open(key, OnSignalingMessageInternal);
         $('#gameid').val(key);
-        
+        mIsView = true;
+        mIsHostController = false;
     };
     
     this.startAsController = function(key)
     {
         sigChan.connect(key, OnSignalingMessageInternal);
+        mIsView = false;
+        mIsHostController = true;
     };
     
     this.disconnect = function()
@@ -42,7 +80,7 @@
         var msg = {};
         msg.tag = lTag;
         msg.content = lContent; 
-        //TODO: Handle tags
+        
         sigChan.sendMessage(JSON.stringify(msg));  
     };
     this.addMessageListener = function(lListener)
@@ -50,21 +88,38 @@
         mMessageListener.push(lListener);
     };
     
+    function OnUserMessage(lTag, lContent)
+    {
+        if(lTag == TAG_ENTER_GAME)
+        {
+            ShowGame(lContent);
+        }
+    }
     
+    
+    function ShowGame(lGameNameUrl)
+    {
+        
+            $('#contentframe').attr("src",  lGameNameUrl);
+            
+            self.Log("opening " +  lGameNameUrl);
+    }
     function OnSignalingMessageInternal(lType, lMsg)
     {
         if(lType == SignalingMessageType.Connected)
         {
-            Log("Connected");
+            self.Log("Connected");
             gConnected = true;
           
           
             $('#openroom').attr("hidden", true);
             $('#joinspan').attr("hidden", true);
             $('#connectedspan').attr("hidden", false);
+            
+            ShowGame("./gamelist.html");
         }else if(lType == SignalingMessageType.UserMessage)
         {
-            Log("Message: " + lMsg);
+            self.Log("Message: " + lMsg);
             var msgObj = JSON.parse(lMsg);
             
             for(var i = 0; i < mMessageListener.length; i++)
@@ -78,9 +133,9 @@
             $('#joinspan').attr("hidden", false);
             $('#connectedspan').attr("hidden", true);
             gConnected = false;
-            Log("Disconnected");
+            self.Log("Disconnected");
         }else{
-            Log("Invalid message received. type: " + lType + " content: " + lMsg);
+            self.Log("Invalid message received. type: " + lType + " content: " + lMsg);
         }
     }
     function GetRandomKey()
@@ -93,5 +148,10 @@
         }
         return result;
     }
-    
+        
+    this.Log = function(msg)
+    {
+        $('#messages').append($('<li>').text(msg));
+        console.debug(msg);
+    }
  }
