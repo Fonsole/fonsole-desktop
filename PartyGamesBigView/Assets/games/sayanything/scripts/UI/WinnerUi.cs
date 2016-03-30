c0304bcfcfe42785108c1df2efe72df6e7b49d5e
jay43k <jay43k@gmail.com>
2016-03-30 18:43:25 -0400

    Start Winning screen transition

---
+++ PartyGamesBigView/Assets/games/sayanything/scripts/UI/WinnerUi.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace PPlatform.SayAnything.Ui
+{
+    /// <summary>
+    /// Handles the transition for presenting the question, the winning answer, and showing scores.
+    /// </summary>
+    public class WinnerUi : MonoBehaviour {
+
+        public GameObject _QuestionPanel;
+        public GameObject _WinnerPanel;
+        public GameObject _ScorePanel;
+
+        void OnEnable()
+        {
+            // Immediately show question panel
+            _QuestionPanel.SetActive(true);
+            // Wait then show winning answer panel
+            StartCoroutine(ShowWinnerPanel());
+        }
+
+        void FixedUpdate()
+        {
+            // Show Score Panel on state change
+            SharedData data = SayAnythingUi.Instance.CurrentData;
+            if (data.state == GameState.ShowScore)
+            {
+                if (!_ScorePanel.activeSelf)
+                    _ScorePanel.SetActive(true);
+            }
+        }
+
+        IEnumerator ShowWinnerPanel()
+        {
+            yield return new WaitForSeconds(1f);
+            _WinnerPanel.SetActive(true);
+        }
+    }
+}
