9df242c9604fb574ac2887e1036899044420bda7
William Curtis <william.curtis@gmail.com>
2016-05-04 23:23:18 +0200

    making the fonsole.com button open that page in the browser

---
+++ PartyGamesBigView/Assets/pplatform/VisitURL.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace PPlatform.Helper
+{
+    public class VisitURL : MonoBehaviour
+    {
+        public string URL;
+
+        public void NavigateToURL()
+        {
+            Application.OpenURL(URL);
+        }
+    }
+
+}
