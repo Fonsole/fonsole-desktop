1ae6b3197bd506fe0a8b3f8f66d6b31bbf56b1fb
ark120202 <ark120202@gmail.com>
2017-07-28 21:46:11 +0500

    Fixed eslint errors in elect.js file

--- elect.js
+++ elect.js
@@ -1,30 +1,30 @@
-const {app, BrowserWindow} = require('electron')
+const { app, BrowserWindow } = require('electron');
 
 let win = null;
 
-app.on('ready', function () {
+app.on('ready', () => {
   // Initialize the window to our specified dimensions
-  win = new BrowserWindow({width: 1280, height: 720, resizable: false});
+  win = new BrowserWindow({ width: 1280, height: 720, resizable: false });
 
-  win.setMenu(null)
+  win.setMenu(null);
 
   // Specify entry point to default entry point of vue.js
   win.loadURL('http://localhost:8080');
 
   // Remove window once app is closed
-  win.on('closed', function () {
+  win.on('closed', () => {
     win = null;
   });
 });
-//create the application window if the window variable is null
+// create the application window if the window variable is null
 app.on('activate', () => {
   if (win === null) {
-    createWindow()
+    createWindow();
   }
-})
-//quit the app once closed
-app.on('window-all-closed', function () {
-  if (process.platform != 'darwin') {
+});
+// quit the app once closed
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
     app.quit();
   }
 });
