de6fb25f9b0de744769c43a6a6d78bcc41f00f46
ark120202 <ark120202@gmail.com>
2017-07-29 13:23:23 +0500

    Started migration to fonsole-networking module

--- src/main.js
+++ src/main.js
@@ -1,29 +1,38 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import NetworkingAPI from 'fonsole-networking/client';
 import Vue from 'vue';
 import Vuex from 'vuex';
 
 import App from './App';
 import Localization from './localization';
 import router from './router';
-import { PPlatform } from './network/pplatformclient';
 
 Vue.use(Vuex);
 Vue.config.productionTip = false;
 
-const gPlatform = new PPlatform();
+const networking = new NetworkingAPI();
 const store = new Vuex.Store({
   state: {
-    currentGame: 'menu',
+    roomName: '',
   },
   mutations: {
-    injectPlatform: (state, frame) => {
-      gPlatform.injectAPI(frame.contentWindow);
+    setRoomName: (state, roomName) => {
+      state.roomName = roomName;
     },
-    createRoom: (/* state, roomName */) => {
-      gPlatform.join();
+    injectPlatform: (state, frame) => {
+      console.log(networking.gameEmitToPlayer);
+      frame.contentWindow.gameEmitToPlayer = networking.gameEmitToPlayer.bind(networking);
+      frame.contentWindow.gameEmitToEveryone = networking.gameEmitToEveryone.bind(networking);
+      frame.contentWindow.gameOn = networking.gameOn.bind(networking);
+      frame.contentWindow.gameOnce = networking.gameOnce.bind(networking);
+      frame.contentWindow.platformType = networking.platform;
     },
   },
+});
+networking.once('room-joined', (roomName) => {
+  alert(roomName);
+  store.commit('setRoomName', roomName);
 });
 
 Vue.use(Localization, store);
