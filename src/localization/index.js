b80d4f8d6709c3b7280a863f1ff7b028f851b271
TideS <tidesmain@gmail.com>
2017-07-30 15:40:18 +0000

    Automatic localization detection

--- src/localization/index.js
+++ src/localization/index.js
@@ -2,9 +2,9 @@ import vuexI18n from 'vuex-i18n';
 
 const languages = [
   // First language will be used as default
-  'english',
+  'en',
 
-  'russian',
+  'ru',
 ];
 
 const Localization = {
@@ -12,11 +12,10 @@ const Localization = {
     Vue.use(vuexI18n.plugin, store);
     languages.forEach((language) => {
       // eslint-disable-next-line import/no-dynamic-require
-      const translation = require(`../assets/localization/fonsole_${language}.json`);
+      const translation = require(`../assets/localization/${language}.json`);
       Vue.i18n.add(language, translation);
     });
-    Vue.i18n.set(languages[0]);
-
+    Vue.i18n.set(/(\w+)-?\w*/g.exec(navigator.language)[1]);
     const localize = function $localize(key, options, pluralization) {
       key = `${this.$options.name}_${key}`;
 
