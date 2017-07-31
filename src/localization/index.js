8093de87f8c8b88117a9d713491985341b60fc15
ark120202 <ark120202@gmail.com>
2017-08-01 01:35:00 +0500

    Added comments to localization plugin and added fallback to English language, if we haven't localization for client's language

--- src/localization/index.js
+++ src/localization/index.js
@@ -1,31 +1,37 @@
 import vuexI18n from 'vuex-i18n';
 
 const languages = [
-  // First language will be used as default
   'en',
-
   'ru',
 ];
 
 const Localization = {
   install(Vue, store) {
     Vue.use(vuexI18n.plugin, store);
+    // Load all available localizations
     languages.forEach((language) => {
       // eslint-disable-next-line import/no-dynamic-require
       const translation = require(`../assets/localization/${language}.json`);
       Vue.i18n.add(language, translation);
     });
-    Vue.i18n.set(/(\w+)-?\w*/g.exec(navigator.language)[1]);
+    // Custom localization function, that can be used for interface localization
     const localize = function $localize(key, options, pluralization) {
       key = `${this.$options.name}_${key}`;
-
       return Vue.prototype.$t(key, options, pluralization);
     };
-
     Vue.prototype.$localize = localize;
+
+    // Get all available localizations.
     Vue.getLocalizationList = function getLocalizationList() {
       return languages;
     };
+
+    // Try to detect client language
+    let clientLanguage = /(\w+)-?\w*/g.exec(navigator.language)[1];
+    // If fonsole is not localized for client's language fallback to english
+    if (!(clientLanguage in languages)) clientLanguage = 'en';
+
+    Vue.i18n.set(clientLanguage);
   },
 };
 
